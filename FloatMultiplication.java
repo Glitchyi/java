95233add1f3b9c4eee089d8580383bb17da91b57
Advaith Narayanan <glitchy@Atamai.localdomain>
2022-10-05 09:25:01 +0530

    >>

--- FloatMultiplication.java
+++ FloatMultiplication.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class FloatMultiplication {
+class FloatMultiplication {
     public static void main(String[] args) {
         float a, b;
         Scanner sc = new Scanner(System.in);
