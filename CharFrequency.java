d9b98429bff6bebb0740ef58d21e0b1f195690a3
Glitchyi <advaithnarayanan8@gmail.com>
2022-10-06 22:13:50 +0530

    done for the day

--- CharFrequency.java
+++ CharFrequency.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class CharFrequency {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String str = sc.next();
+        String str = sc.nextLine();
         char ch = sc.next().charAt(0);
         int count = 0;
         for (int i = 0; i < str.length(); i++) {
