e96ec850c5a087eafc3d4f431d9daa1d939a77cd
Advaith Narayanan <glitchy@Atamai.localdomain>
2022-10-06 22:00:06 +0530

    <<

--- CharFrequency.java
+++ CharFrequency.java
@@ -4,7 +4,7 @@ public class CharFrequency {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = sc.next();
-        char ch = 'a';
+        char ch = sc.next().charAt(0);
         int count = 0;
         for (int i = 0; i < str.length(); i++) {
             if (ch == str.charAt(i)) {
